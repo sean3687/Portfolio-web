0e960a012174272270508069d36440adee4e3958
yonghyuj <yonghyun.jin11@gmail.com>
2022-08-12 17:31:37 +0900

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = props => {
             
             <h4>Hi, my Name is,</h4>
             <h1>Sean Jin</h1>
-            <p className = "title-paragraph"><span className="hover-underline-animation">I'm a software engineer based in Irvine, CA</span> sepecializing in building(and ocationally desgining) exceptional website,application, and everything in between</p>
+            <p className = "title-paragraph"><span className="hover-underline-animation">I'm a software engineer based in Irvine, CA</span> speecializing in building(and ocationally desgining) exceptional website,application, and everything in between</p>
             <p className = "title-resume">
                 <a className ="resume-link" href={Resume} target="_blank">Resume</a>
                 <a className="email-direct" href="mailto:yonghyun.jin11@gmail.com" rel="noopener noreferrer" target="_blank">Say Hello</a>
